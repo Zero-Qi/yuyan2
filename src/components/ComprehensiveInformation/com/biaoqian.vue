179d5464c0158fa2f086eb3b3e0f90f378cf40ea
Zero_Qi <1194660649@qq.com>
2021-06-08 01:25:35 +0800

    img

---
+++ src/components/ComprehensiveInformation/com/biaoqian.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="div1">
+    <img src="~@/assets/img/img1.png" alt="" />
+    <div class="div2">已完成计划</div>
+     <div class="div3"><span>32</span><span>项</span><span class="mm">占比:</span><span>16%</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+img{
+  width: 16.666667rem;
+  height: 8.333333rem;
+}
+.div1 {
+
+  position: relative;
+  width: 100px;
+  height: 100px;
+}
+.div2 {
+  width: 200px;
+  position: absolute;
+  z-index: 2;
+  left: 2.833333rem;
+  top: 1rem;
+  color: #fff;
+}
+.div3 {
+  color: #fff;
+  width: 200px;
+  position: absolute;
+  z-index: 2;
+  left: 2.5rem;
+  top: 3.5rem;
+}
+.mm{
+  padding-left: 50px;
+}
+</style>
---
+++ src/components/ComprehensiveInformation/com/maturityStatistics.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="myChart" :style="{ width: '300px', height: '300px' }"></div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "hello",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  mounted() {
+    this.drawLine(); // 初始化
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("myChart"));
+      // 绘制图表
+      myChart.setOption({
+        title: {
+          text: "",
+          left: "20px",
+          textStyle: {
+            color: "#436EEE",
+            fontSize: 17,
+          },
+        },
+        tooltip: {
+          trigger: "axis",
+        },
+        legend: {
+          itemWidth: 15,
+          itemHeight: 15,
+          data: ["初始TRL", "中期TRL提升值",'目标TRL差距值'],
+        },
+        xAxis: {
+          data: ["技术一", "技术一", "技术一", "技术一", "技术一", "技术一"],
+          splitLine: {
+            show: false,
+          },
+          grid: {
+            left: "10%",
+            bottom: "35%",
+          },
+          axisLabel: {
+            interval: 0,
+            rotate: -40,
+          },
+          axisLine:{
+            symbol : 'none'
+          },
+          axisTick:{
+            show : false
+          }
+        },
+        yAxis: {
+          splitLine: {
+            show: true,
+             lineStyle: {
+                    color: ['#6E7D9C'],
+                    width: 1,
+                    type: 'solid',
+                    opacity: 0.5
+                    
+                }
+          },
+        },
+        series: [
+          {
+            name: "初始TRL",
+            type: "bar",
+            stack: "使用情况",
+            data: [5, 20, 36, 10, 10, 20],
+            itemStyle: {
+              normal: { color: "#2878CA", barBorderRadius: [0, 0, 0, 0] },
+            },
+            barMaxWidth: '40%'
+          },
+          {
+            name: "中期TRL提升值",
+            type: "bar",
+            stack: "使用情况",
+            data: [40, 22, 18, 35, 42, 40],
+            itemStyle: {
+              normal: { color: "#3EBCE8" ,barBorderRadius: [30,30,0,0]},
+            },
+          },
+        ],
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
