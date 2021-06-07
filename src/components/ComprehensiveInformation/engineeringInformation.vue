3ffd69baa92a14ad192a2fdb65e37ff2356ad034
Zero_Qi <1194660649@qq.com>
2021-06-08 01:25:35 +0800

    img

--- src/components/ComprehensiveInformation/engineeringInformation.vue
+++ src/components/ComprehensiveInformation/engineeringInformation.vue
@@ -1,38 +1,38 @@
 <template>
-    <div id="main">
-        <a-card class="topLeft">
-      <div class="assignmentOne">
-        <span class="one">任务总数</span><span class="two"></span>
-      </div>
-      <div class="assignmentTwo">
-        <span class="one">已完成任务</span><span class="two"></span>
-      </div>
-    </a-card>
+  <div id="container">
+    <p>法一：</p>
+    <div class="imgbox">
+      <img src="img1.png" />
+      <lable>空山新雨后，天气晚来秋</lable>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "hello",
   data() {
-    return {
-      
-    };
+    return {};
   },
-  mounted() {
-    
-  },
-  methods: {
-  
-    
-  },
+  mounted() {},
+  methods: {},
 };
 </script>
 
 <style scoped>
-.assignmentOne{
-  width: 260px;
-  height: 100px;
-  background-image: url('img1.png') ;
-}
+#container .imgbox{
+        height: 216px;
+        width: 384px;
+    }
+#container img {
+        position: absolute;/*设为绝对定位*/
+        opacity: 0.7;/*设置透明度*/
+    }
+#container lable {
+        width: inherit;
+        text-align: right;
+        font-size: 20px;
+        color: ivory;
+        position: absolute;/*设为绝对定位*/
+    }
 </style>
